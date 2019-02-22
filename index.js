719ef85dc3f821eac6ba1a86cdde223d9bdf0064
Alexandre Müller Júnior <alexandremj@protonmail.com>
2019-02-21 21:28:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.get('/api/posts', (req, res) => {
     res.send(posts);
-})
+});
 
 // Show single post
 app.get('/api/posts/:id', (req, res) => {
@@ -28,7 +28,56 @@ app.get('/api/posts/:id', (req, res) => {
     const post = posts.find(p => p.id === id);
     if (!post) res.status(404).send("The post with the given ID was not found");
     res.send(post);
+});
+
+// Create
+app.post('api/posts', (req, res) => {
+    const title, body = req.body;
+
+    const post = {
+        userId: 1,
+        id: posts.length +1,
+        title,
+        body
+    };
+
+    // add post
+    posts.push(post);
+
+    // return post
+    res.send(post);
+});
+
+// Update route
+app.put('/api/posts/:id', (req, res) => {
+    // Grab post
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    // Check if exists
+    if (!post) res.status(404).send("The post with the given ID was not found.");
+
+    // Update post
+    post.title = req.body.title;
+    post.body = req.body.body;
+
+    // Return post
+    res.send(post);
 })
+
+// Destroy 
+app.delete('/api/posts/:id', (req, res) => {
+    // Grab post
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    // Delete post
+    const index = posts.indexOf(post);
+    posts.splice(index, 1);
+
+    // Return post
+    res.send(post);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
